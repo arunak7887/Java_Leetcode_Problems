5dd7ab127a1f83c777d23f212309b75dd1c0fa5c
Arun Budhaji kinwad <85956491+arunak7887@users.noreply.github.com>
2024-02-12 23:35:46 +0530

    Create Animal.java

---
+++ Oops/Animal.java
@@ -0,0 +1,10 @@
+Class Animal{
+  public void eat(){
+    System.out.println("I am Esating");
+  }
+  public static void main(String args[]){
+    Animal bg=new Animal();
+    bg.eat()
+  }
+}
+    
