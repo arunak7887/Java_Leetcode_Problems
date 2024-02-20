acd72ca8333741a7ab9bc4b1adb1a21cfb9f1375
Arun Budhaji kinwad <85956491+arunak7887@users.noreply.github.com>
2024-02-20 17:49:19 +0530

    Create Test.java

---
+++ If_else/Test.java
@@ -0,0 +1,12 @@
+public class Test{
+ public static void main(String args[]){
+   int x=30;
+   if(x<20){
+     System.out.println("This is if Statement");
+   }else{
+     System.out.println("This is else Statement");
+   }
+ }
+}
+//output:This is else Statement
+     
